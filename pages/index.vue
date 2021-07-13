d21c748cedbc49e330679aa446451a8d16250c6f
jahidanowar <milon5143@gmail.com>
2021-07-13 15:09:20 +0530

    feat: Initial pages scaffold

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <!-- Hero Section -->
-    <section class="container mx-auto py-10 md:py-28">
-      <div class="flex flex-wrap justify-between items-center">
+    <section class="hero py-10 md:py-12">
+      <div
+        class="container mx-auto flex flex-wrap justify-between items-center"
+      >
         <div class="w-full md:w-1/2 mb-10 text-center md:text-left">
-          <h1 class="text-4xl md:text-5xl font-bold leading-tight">
+          <h1 class="text-4xl md:text-6xl font-bold leading-tight">
             we help you to build
             <span class="text-indigo-500 dark:text-indigo-600"
               >your career</span
@@ -18,11 +20,89 @@
           <button class="btn btn-primary mt-5">learn now</button>
         </div>
         <div class="w-full md:w-1/2">
-          <img src="~/assets/img/bengalcoder-hero-image.svg" width="400px" />
+          <!-- <img src="~/assets/img/bengalcoder-hero-image.svg" width="400px" /> -->
         </div>
       </div>
     </section>
     <!-- ./ Hero Section  -->
+    <!-- Blurb Section  -->
+    <section class="blurb py-10 md:py-20">
+      <div class="container mx-auto">
+        <h1 class="text-center text-4xl font-bold">we teach you to</h1>
+        <div class="flex mt-16 gap-10">
+          <div class="w-full md:w-1/3">
+            <div class="px-10 py-12 rounded-lg border text-center">
+              <svg
+                class="w-12 h-12 mx-auto"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                ></path>
+              </svg>
+              <h3 class="text-2xl font-bold my-3">program</h3>
+              <p>
+                our project base programming tutorial is as easy as learning to
+                a ride bicycle.
+              </p>
+            </div>
+          </div>
+          <div class="w-full md:w-1/3">
+            <div class="px-10 py-12 rounded-lg border text-center">
+              <svg
+                class="w-12 h-12 mx-auto"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                ></path>
+              </svg>
+              <h3 class="text-2xl font-bold my-3">program</h3>
+              <p>
+                our project base programming tutorial is as easy as learning to
+                a ride bicycle.
+              </p>
+            </div>
+          </div>
+          <div class="w-full md:w-1/3">
+            <div class="px-10 py-12 rounded-lg border text-center">
+              <svg
+                class="w-12 h-12 mx-auto"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                ></path>
+              </svg>
+              <h3 class="text-2xl font-bold my-3">program</h3>
+              <p>
+                our project base programming tutorial is as easy as learning to
+                a ride bicycle.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- ./ Blurb Section -->
   </div>
 </template>
 
